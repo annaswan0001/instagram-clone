0508909620bf16c726b68444613135065289bdd1
Anna Samoylenko <annasamoylenko0001@gmail.com>
2021-03-11 17:52:25 +0200

    delete unnecessary files, add basic styles, logo to App

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="headerImg"
+          className="app__headerImg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
