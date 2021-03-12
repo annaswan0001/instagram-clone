c5fe9d3625314dccf726c9c53786b86c4e77b92e
Anna Samoylenko <annasamoylenko0001@gmail.com>
2021-03-12 21:47:58 +0200

    create  Upload Image Component  with styles and logic

--- src/components/ImageUpload/ImageUpload.jsx
+++ src/components/ImageUpload/ImageUpload.jsx
@@ -1,7 +1,9 @@
 import Button from "@material-ui/core/Button";
 import { useState } from "react";
-
-const ImageUpload = () => {
+import { db, storage } from "../../firebase";
+import firebase from "firebase";
+import './ImageUpload.css'
+const ImageUpload = ({ userName }) => {
   const [caption, setCaption] = useState("");
   const [image, setImage] = useState(null);
   const [progress, setProgress] = useState(0);
@@ -11,9 +13,41 @@ const ImageUpload = () => {
       setImage(e.target.files[0]);
     }
   };
-  
+
+  const handleUpload = (e) => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapchot) => {
+        const prs = Math.round(
+          (snapchot.bytesTransferred / snapchot.totalBytes) * 100
+        );
+        setProgress(prs);
+      },
+      (error) => alert(error.message),
+      () => {
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("posts").add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              caption,
+              imgUrl: url,
+              userName,
+            });
+            setProgress(0);
+            setCaption("");
+            setImage(null);
+          });
+      }
+    );
+  };
+
   return (
-    <div>
+    <div className="imageUpload">
+      <progress className="imageUpload__progress" value={progress} max="100" />
       <input
         type="text"
         placeholder="Enter a caption..."
@@ -21,7 +55,7 @@ const ImageUpload = () => {
         onChange={(e) => setCaption(e.target.value)}
       />
       <input type="file" onChange={handleChange} />
-      <Button onClick={handleUpload}>Upload</Button>
+      <Button type="outline" onClick={handleUpload}>Upload</Button>
     </div>
   );
 };
