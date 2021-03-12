89de0d1e9fd35e7028a6820da113e57014f538a3
Anna Samoylenko <annasamoylenko0001@gmail.com>
2021-03-12 15:07:24 +0200

    add getData logic  from fireBase and show posts in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import "./App.css";
-
+import Post from "./components/Post/Post";
+import { useEffect, useState } from "react";
+import { db } from "./firebase";
+import Modal from '@material-ui/core/Modal';
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapShot) => {
+      setPosts(
+        snapShot.docs.map((doc) => {
+          return { ...doc.data(), userId: doc.id };
+        })
+      );
+    });
+  }, []);
+
+  console.log(posts);
+
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        Руддщ
+      </Modal>
       <div className="app__header">
         <img
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
@@ -10,6 +35,10 @@ function App() {
           className="app__headerImg"
         />
       </div>
+      {posts.length > 0 &&
+        posts.map((post) => {
+          return <Post key={post.userId} postData={post} />;
+        })}
     </div>
   );
 }
